9eb8d2e4a9c298ea75d6963c8e88a99d7ef88c57
Vasrevenge <simonigiacomo94@gmail.com>
2025-03-11 16:30:44 +0100

    D: Initial functions

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+filepath = str()
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book_string = f.read()
+
+    return book_string
+
+def main():
+    filepath = "~/progetti/BookBot/books/frankenstein.txt"
+
+
+main()
