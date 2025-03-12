af52ff3239149e33173c0ea4e54754b2eb2c5646
Vasrevenge <simonigiacomo94@gmail.com>
2025-03-12 15:26:19 +0100

    Bookbot almost done

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
-from stats import count_words,count_characters
+from stats import count_words,count_characters,dictionary_presentation
 file_path = str()
-# r"/home/jack/progetti/BookBot/books/frankenstein.txt"
 
 def main():
     file_path = r"/home/jack/progetti/BookBot/books/frankenstein.txt"
     content = get_book_text(file_path)
     word_number = count_words(content)
 
-    c_set = count_characters(content)
-
-    print(word_number,"words found in the document")
-    print(c_set)
+    c_dict = count_characters(content)
+    c_set = dictionary_presentation(c_dict)
+    new_list = list() 
+    
+    #print("============ BOOKBOT ============")
+    #print("Analyzing book found at :", file_path)
+    #print("----------- Word Count ----------")
+    print('Found',word_number,'total words')
+    #print("--------- Character Count -------")
+    for d in c_set:
+        a = str(d[0])
+        if a.isalpha():
+            print( d[0]+":", d[1])
+            
 
 def get_book_text(file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -6,11 +6,23 @@ def count_words(content):
 def count_characters(content):
     character_list = (content).lower()
     character_set = set(character_list)
-    dictionary = dict()
+    dictionary = {}
 
     for i in character_set:
         count =  character_list.count(i)
-        dictionary.update({i : count})        
-
+        dictionary.update({i : count})
+        
     return dictionary
+
+
+def dictionary_presentation(dictionary):
+    dict_list = list()
+
+    for (key, value) in enumerate(dictionary.items()):
+        dict_list.append(value)
+    
+    sorted_dict_list = sorted(dict_list, key=lambda x: (x[1]), reverse=True)
+
+    return sorted_dict_list
+
 
