7d8441f528c9a01efcef85b84357f12cc41aa923
Vasrevenge <simonigiacomo94@gmail.com>
2025-03-12 15:36:53 +0100

    dynamic file selection

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
+import sys
 from stats import count_words,count_characters,dictionary_presentation
 file_path = str()
 
 def main():
-    file_path = r"/home/jack/progetti/BookBot/books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage : python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file_path = sys.argv[1]                     #r"/home/jack/progetti/BookBot/books/frankenstein.txt"
     content = get_book_text(file_path)
     word_number = count_words(content)
 
@@ -11,16 +17,15 @@ def main():
     c_set = dictionary_presentation(c_dict)
     new_list = list() 
     
-    #print("============ BOOKBOT ============")
-    #print("Analyzing book found at :", file_path)
-    #print("----------- Word Count ----------")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at :", file_path)
+    print("----------- Word Count ----------")
     print('Found',word_number,'total words')
-    #print("--------- Character Count -------")
+    print("--------- Character Count -------")
     for d in c_set:
         a = str(d[0])
         if a.isalpha():
-            print( d[0]+":", d[1])
-            
+            print( d[0]+":", d[1])            
 
 def get_book_text(file_path):
     with open(file_path) as f:
